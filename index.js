07c85a1bf06698c195f05888954096f3a7fbe9f7
Diana Discher <dianadischer@dianas-mbp.myfiosgateway.com>
2024-01-14 16:01:24 -0500

    separated the shape constructors into their own module and created structure for testing suite

--- index.js
+++ index.js
@@ -4,53 +4,8 @@ const inquirer = require("inquirer");
 // import filesystem to write svg files
 const fs = require("fs");
 
-// Shape constructor
-class Shape {
-    constructor(text, textColor, shapeColor) {
-      this.text = text;
-      this.textColor = textColor;
-      this.shapeColor= shapeColor;
-    }
-}
-
-// Triangle constructor inherits shape and adds svg function that specifically creates an svg triangle
-class Triangle extends Shape {
-    constructor(text, textColor, shapeColor) {
-      super(text, textColor, shapeColor);
-    }
-    // extends shape by writing svg using user input
-    svg() {
-        return `<svg width="100" height="100"> <polygon points="50,13.397 100,100 0,100" fill="${this.shapeColor}" />
-        <text x="50" y="73" font-size="20" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
-        </svg>`
-    } 
-}
-
-// Square constructor inherits shape and adds svg function that specifically creates an svg square
-class Square extends Shape {
-    constructor(text, textColor, shapeColor) {
-      super(text, textColor, shapeColor);
-    }
-    // extends shape by writing svg using user input
-    svg() {
-        return `<svg width="100" height="100"> <rect width="100" height="100" fill="${this.shapeColor}" />
-        <text x="50" y="55" font-size="20" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
-        </svg>`
-    } 
-}
-
-// Circle constructor inherits shape and adds svg function that specifically creates an svg circle
-class Circle extends Shape {
-    constructor(text, textColor, shapeColor) {
-      super(text, textColor, shapeColor);
-    }
-    // extends shape by writing svg using user input
-    svg() {
-        return `<svg width="100" height="100"> <circle cx="50" cy="50" r="40" fill="${this.shapeColor}" />
-        <text x="50" y="55" font-size="20" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
-        </svg>`
-    } 
-}
+// import shapes
+const { Triangle, Square, Circle } = require("./lib/shapes.js")
 
 // Prompts to ask user through inquirer
 function init() {
@@ -89,21 +44,21 @@ function init() {
             // construct triangle
             let logo = new Triangle(response.textInput, response.textColorInput, response.shapeColorInput);
             // use the svg function that returns svg code as file content
-            fs.writeFile("./examples/logo.svg", logo.svg(), ()=> console.log('Your logo is in the examples folder!'));
+            fs.writeFile("./examples/logo.svg", logo.render(), ()=> console.log('Your logo is in the examples folder!'));
         
         // creates square svg, writes it to logo.svg in examples folder, and tells user
         } else if (response.shapeInput == "Square") {
             // construct square
             let logo = new Square(response.textInput, response.textColorInput, response.shapeColorInput);
             // use the svg function that returns svg code as file content
-            fs.writeFile("./examples/logo.svg", logo.svg(), ()=> console.log('Your logo is in the examples folder!'));
+            fs.writeFile("./examples/logo.svg", logo.render(), ()=> console.log('Your logo is in the examples folder!'));
         
             // creates circle svg, writes it to logo.svg in examples folder, and tells user
         } else if (response.shapeInput == "Circle") {
             // construct circle
             let logo = new Circle(response.textInput, response.textColorInput, response.shapeColorInput);
             // use the svg function that returns svg code as file content
-            fs.writeFile("./examples/logo.svg", logo.svg(), ()=> console.log('Your logo is in the examples folder!'));
+            fs.writeFile("./examples/logo.svg", logo.render(), ()=> console.log('Your logo is in the examples folder!'));
         
         // if none of the above responses went through, there was an issue and the program restarts
         } else {
